40e25f0aaa94e7afeb4d1a3f3f3a4fd2d484c599
ĐẶNG NGỌC KHIÊU <dangngockhieu28082005@gmail.com>
2024-09-19 10:37:00 +0700

    Add files via upload

---
+++ remove Nth Node from end of List.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+	struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+	void add(ListNode* head,int x){
+		if(!head){
+			head->val=x;
+		}
+		else{
+		ListNode* tmp=head;
+		ListNode* a=new ListNode(x);
+		while(tmp->next!=nullptr){
+			tmp=tmp->next;
+		}
+		tmp->next=a;
+	    }
+	}
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+   
+        for (int i = 0; i < n; ++i) {
+            fast = fast->next;
+        }
+        
+        if (!fast) {
+            return head->next;
+        }
+        
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return head;
+    }
+};
+int main(){
+	return 0;
+}
